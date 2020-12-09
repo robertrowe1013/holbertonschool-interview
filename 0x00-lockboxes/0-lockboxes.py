0573a96353b9122b6924a5e6651412ab32c48b5d
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-09 15:24:07 -0600

    added comments, moved no boxes check

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,19 +2,19 @@
 
 
 def canUnlockAll(boxes):
-    """unlock the box"""
+    """key for box 0, check for no boxes, unlock box 0"""
     unlockedBoxes = set([0])
+    if boxes is None or len(boxes) <= 1:
+        return True
     for keys in boxes[0]:
         openBox(keys, unlockedBoxes, boxes)
-    if boxes is None or len(boxes) < 1:
-        return True
     if len(boxes) == len(unlockedBoxes):
         return True
     return False
 
 
 def openBox(keys, unlockedBoxes, boxes):
-    """open box"""
+    """open box, if contains key to unopened box, open that box"""
     if keys < (len(boxes)) and keys not in unlockedBoxes:
         unlockedBoxes.add(keys)
         for keys in boxes[keys]:
