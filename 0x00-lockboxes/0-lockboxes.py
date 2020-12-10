4aef18898f06dc9ce7adae0bfcf387b1678ff488
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-10 00:22:34 -0600

    without recursion

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,6 +2,7 @@
 
 
 def canUnlockAll(boxes):
+    '''
     """key for box 0, check for no boxes, unlock box 0"""
     unlockedBoxes = set([0])
     if boxes is None or len(boxes) <= 1:
@@ -11,6 +12,17 @@ def canUnlockAll(boxes):
     if len(boxes) == len(unlockedBoxes):
         return True
     return False
+    '''
+    """without recusion"""
+    unlockedBoxes = [0]
+    openBoxes = 1
+
+    for box in unlockedBoxes:
+        for key in boxes[box]:
+            if key < len(boxes) and key not in unlockedBoxes:
+                unlockedBoxes.append(key)
+                openBoxes += 1
+    return openBoxes == len(boxes)
 
 
 def openBox(keys, unlockedBoxes, boxes):
