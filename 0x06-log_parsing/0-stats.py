95282cfd455c84b949a3d5b2a4a336a326c74e1a
robertrowe1013 <robertrowe1013@gmail.com>
2021-01-29 07:33:07 +0000

    pep8

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,10 +1,13 @@
+#!/usr/bin/python3
 """ log parse """
 import sys
 import signal
 
 size = 0
 i = 1
-status_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+status_dict = {'200': 0, '301': 0, '400': 0, '401': 0,
+               '403': 0, '404': 0, '405': 0, '500': 0}
+
 
 def printall(sig=None, frame=None):
     """ print it """
