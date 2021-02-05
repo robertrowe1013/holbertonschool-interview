f580e5f5600b956d68eb74a2d2052fdcb84a6729
robertrowe1013 <robertrowe1013@gmail.com>
2021-02-05 14:19:46 +0000

    finish

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -12,10 +12,23 @@
 
 int check_cycle(listint_t *list)
 {
+	listint_t *tort = list;
+	listint_t *hare = list;
+
 	if (list == NULL)
 	{
 		return (0);
 	}
+	while (tort != NULL && hare != NULL && hare->next != NULL)
+	{
+		tort = tort->next;
+		hare = hare->next->next;
+		if (tort == hare)
+		{
+			return (1);
+		}
+	}
+
 	return (0);
 }
 
