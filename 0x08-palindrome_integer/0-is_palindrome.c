86769505a7a21973b766bd3124fbb7cb5380f742
robertrowe1013 <robertrowe1013@gmail.com>
2021-02-10 17:27:55 +0000

    first real try

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,3 +1,4 @@
+
 #include "palindrome.h"
 
 /**
@@ -10,9 +11,27 @@
 
 int is_palindrome(unsigned long n)
 {
+	unsigned long reverse = 0;
+	unsigned long remainder = 0;
+	unsigned long temp = n;
+
 	if (n < 10)
 	{
 		return (1);
 	}
+	while (temp != 0)
+	{
+		printf("start loop\n");
+		printf("temp: %lu, remainder: %lu, reverse: %lu\n", temp, remainder, reverse);
+		remainder = temp % 10;
+		reverse = reverse * 10 + remainder;
+		temp /= 10;
+		printf("end loop, temp: %lu, remainder: %lu, reverse: %lu\n", temp, remainder, reverse);
+	}
+	if (reverse == n)
+	{
+		return (1);
+	}
+
 	return (0);
 }
