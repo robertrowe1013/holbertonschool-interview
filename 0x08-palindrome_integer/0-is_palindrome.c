6b53f33aba286106df785be195b6847715e0b922
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-02-10 13:24:07 -0600

    test

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -19,7 +19,16 @@ int is_palindrome(unsigned long n)
 	{
 		return (1);
 	}
+	while (temp != 0)
+	{
+		remainder = temp % 10;
+		reverse = reverse * 10 + remainder;
+		temp /= 10;
+	}
+	if (reverse == n)
+	{
+		return (1);
+	}
 
 	return (0);
 }
-
