d0bf933cbcd641a45c9cab8b7eb14ca6911e9aaf
robertrowe1013 <robertrowe1013@gmail.com>
2021-02-18 06:16:49 +0000

    more checks

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -8,5 +8,9 @@ def validUTF8(data):
 
     if type(data) is not list:
         return False
-    else:
-        return True
+    for num in data:
+        if type(num) is not int:
+            return False
+        if num > 255:
+            return False
+    return True
