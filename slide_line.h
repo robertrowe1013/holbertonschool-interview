982466c0b03eb7b2f01dd46ae11362840a752f3c
robertrowe1013 <robertrowe1013@gmail.com>
2021-02-24 20:09:29 +0000

    header

---
+++ slide_line.h
@@ -0,0 +1,10 @@
+#ifndef SLIDE_LINE_H
+#define SLIDE_LINE_H
+#include <stdio.h>
+
+#define SLIDE_LEFT
+#define SLIDE_RIGHT
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif /* SLIDE_LINE_H */
