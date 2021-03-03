e67a6fb28f2db78f411324affa0e40dd8cb2d329
robertrowe1013 <robertrowe1013@gmail.com>
2021-03-03 03:39:16 +0000

    start

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,19 @@
+#include "menger.h"
+
+/**
+  * menger - draw a sierpinski carpet
+  * @level: fractal level
+  *
+  */
+
+void menger(int level)
+{
+	if (isdigit(level))
+	{
+		if (level == 0)
+			printf("#\n");
+		if (level == 1)
+			printf("###\n# #\n###\n");
+	}
+}
+
