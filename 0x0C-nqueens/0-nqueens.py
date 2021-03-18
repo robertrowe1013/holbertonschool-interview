6aa13d1d7eae7ced8d22b41d113b2b5381f205df
robertrowe1013 <robertrowe1013@gmail.com>
2021-03-18 17:10:07 +0000

    checker test

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -16,24 +16,26 @@ def attacked(i, j):
         if board[i][q] == 1 or board[q][j] == 1:
             return True
     for q in range(0, N):
-        for d in range(0, N):
-            if (q + d == i + j) or (q - d == i - j):
-                if board[q][d] == 1:
+        for diag in range(0, N):
+            if (q + diag == i + j) or (q - diag == i - j):
+                if board[q][diag] == 1:
                     return True
     return False
 
 
-def nqueen(n):
+def nqueen(n, start):
     """
     place queen
     """
     if n == 0:
         return True
     for i in range(0, N):
-        for j in range(0, N):
+        if i > 0:
+            start = 0
+        for j in range(start, N):
             if (not(attacked(i, j))) and (board[i][j] != 1):
                 board[i][j] = 1
-                if nqueen(n - 1):
+                if nqueen(n - 1, start):
                     return True
                 board[i][j] = 0
     return False
@@ -53,11 +55,13 @@ if __name__ == "__main__":
         print("N must be at least 4")
         exit(1)
 
-    board = [[0] * N for _ in range(N)]
-    nqueen(N)
-    solution_list = []
     for i in range(0, N):
-        for j in range(0, N):
-            if board[i][j] == 1:
-                solution_list.append([i, j])
-    print(solution_list)    
+        board = [[0] * N for _ in range(N)]
+        nqueen(N, i)
+        solution_list = []
+        for row in range(0, N):
+            for col in range(0, N):
+                if board[row][col] == 1:
+                    solution_list.append([row, col])
+        if solution_list[0][1] == i:
+            print(solution_list)
