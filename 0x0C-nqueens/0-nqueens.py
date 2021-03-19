db6b0f847a01e10a5d82b310ce932e08fe63ef12
robertrowe1013 <robertrowe1013@gmail.com>
2021-03-19 14:55:51 +0000

    ji's solution

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -6,39 +6,30 @@ N Queens
 import sys
 
 
-def attacked(i, j):
+def attacked(queens, i, j):
     """
     is new queen safe
 
     check horizontal/vertical then check diagonals
     """
-    for q in range(0, N):
-        if board[i][q] == 1 or board[q][j] == 1:
-            return True
-    for q in range(0, N):
-        for diag in range(0, N):
-            if (q + diag == i + j) or (q - diag == i - j):
-                if board[q][diag] == 1:
-                    return True
-    return False
+    for q in queens:
+        if q[0] == i or q[1] == j:
+            return False
+        if abs(q[0] - i) == abs(q[1] - j):
+            return False
+    return True
 
 
-def nqueen(n, start):
+def nqueen(rows, queens = [], j = 0):
     """
     place queen
     """
-    if n == 0:
-        return True
-    for i in range(0, N):
-        if i > 0:
-            start = 0
-        for j in range(start, N):
-            if (not(attacked(i, j))) and (board[i][j] != 1):
-                board[i][j] = 1
-                if nqueen(n - 1, start):
-                    return True
-                board[i][j] = 0
-    return False
+    if len(queens) == N:
+        print(queens)
+        return
+    for i in rows:
+        if attacked(queens, i, j):
+            nqueen(rows.difference({i}), queens + [[i, j]], j + 1)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -55,13 +46,4 @@ if __name__ == "__main__":
         print("N must be at least 4")
         exit(1)
 
-    for i in range(0, N):
-        board = [[0] * N for _ in range(N)]
-        nqueen(N, i)
-        solution_list = []
-        for row in range(0, N):
-            for col in range(0, N):
-                if board[row][col] == 1:
-                    solution_list.append([row, col])
-        if solution_list[0][1] == i:
-            print(solution_list)
+    nqueen(set(range(N)))
