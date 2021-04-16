ea9ef9e235f6f92858bb867f61dc06165ff27306
robertrowe1013 <robertrowe1013@gmail.com>
2021-04-16 01:54:56 +0000

    semistandard

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,28 +3,19 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(url, function (error, response, body)
-{
-    if (error)
-    {
-        console.log(error);
-    }
-    else
-    {
-        const chars = JSON.parse(body).characters;
-        for (let i = 0; i < chars.length; i++)
-        {
-            request(chars[i], function (error, response, body)
-            {
-                if (error)
-                {
-                    console.log(error);
-                }
-                else
-                {
-                    console.log(JSON.parse(body).name)
-                }
-            });
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const chars = JSON.parse(body).characters;
+    for (let i = 0; i < chars.length; i++) {
+      request(chars[i], function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
         }
+      });
     }
+  }
 });
