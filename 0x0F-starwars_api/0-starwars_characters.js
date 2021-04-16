462a371f0462969b6b739a6c37b8fc7b3a83f0ee
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-04-15 20:45:15 -0500

    first pass

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,1 +1,30 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, function (error, response, body)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    else
+    {
+        const chars = JSON.parse(body).characters;
+        for (let i = 0; i < chars.length; i++)
+        {
+            request(cahrs[i], function (error, response, body)
+            {
+                if (error)
+                {
+                    console.log(error);
+                }
+                else
+                {
+                    console.log(JSON.parse(body).name)
+                }
+            });
+        }
+    }
+});
