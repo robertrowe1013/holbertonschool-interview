37e8030efd84f23c51886f113f65246213682cf1
robertrowe1013 <robertrowe1013@gmail.com>
2021-04-16 20:34:33 +0000

    changed when tallest is checked

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -10,7 +10,6 @@ def rain(walls):
     leftwall = 0
     rightwall = 0
     water = 0
-    tallest = max(walls)
 
     if len(walls) <= 2:
         return (0)
@@ -25,7 +24,7 @@ def rain(walls):
     water = 0
     leftwall = 0
     rightwall = 0
-    if walls[0] == tallest and walls[len(walls) - 1] == tallest:
+    if walls[0] == max(walls) and walls[len(walls) - 1] == max(walls):
         return (trappedrain)
     walls.reverse()
     for i in range(len(walls)):
