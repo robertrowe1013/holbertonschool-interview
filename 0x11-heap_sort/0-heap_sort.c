6cd2a02d6d53fa758a592e48f987a7b3329444a6
robertrowe1013 <robertrowe1013@gmail.com>
2021-04-22 04:26:04 +0000

    sort 2

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -4,13 +4,26 @@
 /**
   * heap_sort - heap sort, sift-down
   * @array: array to sort
-  * size: size of array
+  * @size: size of array
   */
 
 void heap_sort(int *array, size_t size)
 {
+	int swap;
+
 	if (array == NULL)
 		return;
 	if (size < 2)
 		return;
+	if (size == 2)
+	{
+		if (array[0] >= array[1])
+		{
+			swap = array[0];
+			array[0] = array[1];
+			array[1] = swap;
+			print_array(array, size);
+		}
+		return;
+	}
 }
