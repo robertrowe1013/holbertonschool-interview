82ce742ac26f4221774a9c1e9f41b5a8490d6247
robertrowe1013 <robertrowe1013@gmail.com>
2021-04-22 03:48:34 +0000

    start

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+  * heap_sort - heap sort, sift-down
+  * @array: array to sort
+  * size: size of array
+  */
+
+void heap_sort(int *array, size_t size)
+{
+	if (array == NULL)
+		return;
+	if (size < 2)
+		return;
+}
