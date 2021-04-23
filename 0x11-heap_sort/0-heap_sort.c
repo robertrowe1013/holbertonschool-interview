803b93f21e6c87afb436220ce0750db29f003ced
robertrowe1013 <robertrowe1013@gmail.com>
2021-04-23 01:21:46 +0000

    test

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -9,7 +9,7 @@
 
 void heap_sort(int *array, size_t size)
 {
-	int swap;
+	int tmp, i, arr_size;
 
 	if (array == NULL)
 		return;
@@ -19,11 +19,65 @@ void heap_sort(int *array, size_t size)
 	{
 		if (array[0] >= array[1])
 		{
-			swap = array[0];
+			tmp = array[0];
 			array[0] = array[1];
-			array[1] = swap;
+			array[1] = tmp;
 			print_array(array, size);
 		}
 		return;
 	}
+	arr_size = size;
+	for (i = (arr_size / 2) - 1; i >= 0; i--)
+		heapify(array, size, i);
+	for (i = arr_size - 1; i >= 0; i--)
+	{
+		swap(array, &array[0], &array[i]);
+		heapify(array, i, 0);
+	}
+}
+
+/**
+  * heapify - heapify
+  * @arr: array
+  * @size: size
+  * @root: root
+  */
+
+void heapify(int *arr, size_t size, size_t root)
+{
+	int left = (root * 2) + 1;
+	int right = (root * 2) + 2;
+	int parent = root;
+	int arr_size = size;
+
+	if (left < arr_size && arr[left] > arr[parent])
+		parent = left;
+	if (right < arr_size && arr[right] > arr[parent])
+		parent = right;
+	if (parent != (int)root)
+	{
+		swap(arr, &arr[root], &arr[parent]);
+		heapify(arr, size, parent);
+	}
+}
+
+/**
+  * swap - swap
+  * @arr: array
+  * @parent: parent
+  * @child: child
+  * @arr_size: array size
+  */
+
+void swap(int *arr, int *parent, int *child)
+{
+	int tmp;
+	size_t i;
+
+	tmp = *parent;
+	*parent = *child;
+	*child = tmp;
+	for (i = 0; arr[i]; i++)
+		continue;
+	print_array(arr, i);
 }
--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -3,5 +3,7 @@
 
 void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
+void heapify(int *arr, size_t size, size_t root);
+void swap(int *arr, int *parent, int *child);
 
 #endif /* SORT_H */
