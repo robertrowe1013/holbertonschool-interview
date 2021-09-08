18ab0da7ca82dacfeb980bfe3e0464c2bb464b38
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-09-07 23:52:08 -0500

    test bad subreddit request

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-"""Query reddit API for work count in hot list"""
+"""Query reddit API for work count in hot list using recusion"""
+import requests
 
 
-def count_words(subreddit, word_list):
+def count_words(subreddit, word_list, next=None, count={}):
     """Count words in word_list in subreddit"""
-
-    if r.status_code != 200:
-        return None
+    if next is None:
+        subreddit_URL = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    else:
+        subreddit_URL = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit, next)
+    subreddit_req = requests.get(subreddit_URL, headers={"user-agent": "user"}, allow_redirects=False)
+    try:
+        data = subreddit_req.json().get("data")
+    except:
+        return
+    
