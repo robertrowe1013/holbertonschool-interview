55ed45ddc98049a4dad63e837c1545c2d67bfd80
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-09-08 00:32:11 -0500

    get count results to be parsed

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -3,17 +3,31 @@
 import requests
 
 
-def count_words(subreddit, word_list, next=None, count={}):
+def count_words(subreddit, word_list, after=None, count={}):
     """Count words in word_list in subreddit"""
     if next is None:
         subred_URL = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     else:
         subred_URL = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
             subreddit, next)
-    subreddit_req = requests.get(subreddit_URL,
+    subreddit_req = requests.get(subred_URL,
                                  headers={"user-agent": "user"},
                                  allow_redirects=False)
     try:
         data = subreddit_req.json().get("data")
     except:
         return
+    for word in word_list:
+        word = word.lower()
+        if word not in count.keys():
+            count[word] = 0
+    children = data.get("children")
+    for child in children:
+        title = (child.get("data").get("title").lower())
+        title = title.split(' ')
+        for word in word_list:
+            word = word.lower()
+            count[word] += title.count(word)
+    after = data.get("after")
+    if after is not None:
+        return count_words(subreddit, word_list, after, count)
