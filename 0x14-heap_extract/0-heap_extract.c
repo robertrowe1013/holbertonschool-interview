4aa0f19fbd5c070494aba5a7cee663b27177a410
robertrowe1013 <robertrowe1013@gmail.com>
2021-09-16 20:54:32 +0000

    test single node heap

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -9,7 +9,43 @@
 
 int heap_extract(heap_t **root)
 {
-	if (root == NULL)
+	int value;
+
+	if (root == NULL || *root == NULL)
+	{
 		return (0);
-	return (0);
+	}
+	value = (*root)->n;
+	if ((*root)->left == NULL && (*root)->right == NULL)
+	{
+		*root = NULL;
+	}
+
+	return (value);
+}
+
+
+
+void heapify(heap_t *node)
+{
+	int tmp;
+	heap_t *max = node;
+
+	if (node->left != NULL && node->left->n > max->n)
+	{
+		max = node->left;
+	}
+
+	if (node->right != NULL && node->right->n > max->n)
+	{
+		max = node->right;
+	}
+
+	if (max != node)
+	{
+		tmp = node->n;
+		node->n = max->n;
+		max->n = tmp;
+		heapify(max);
+	}
 }
