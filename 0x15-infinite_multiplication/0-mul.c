bb71f67c1b8defe1ec1b95b4ad86fc5e0c1e78f5
robertrowe1013 <robertrowe1013@gmail.com>
2021-09-23 03:02:34 +0000

    start

---
+++ 0x15-infinite_multiplication/0-mul.c
@@ -0,0 +1,78 @@
+#include "holberton_h"
+
+/**
+  * main - multiplies two positive numbers
+  * @argc: number of arguments
+  * @argv: arguments
+  *
+  * Return: 0 else 98 on error
+  */
+int main(int argc, int *argv[])
+{
+	if (argc != 3 || _isdigit(argv[1] != 0) || _isdigit(argv[2]) != 0)
+	{
+		_puts("Error");
+		exit(98);
+	}
+}
+
+/**
+  * _puts: prints a string
+  * @str: string to print
+  *
+  * Return: 0 else 1 on error
+  */
+int _puts(char *str)
+{
+	int i;
+	int len = _strlen(str);
+
+	if (str == NULL)
+	{
+		return (1);
+	}
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	return (0);
+}
+
+/**
+  * _isdigit: verifies if a string is numerical
+  * @str: string to verify
+  *
+  * Return: 0 if digit, else 1
+  */
+int _isdigit(char *str)
+{
+	int i;
+	int len = _strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+
+/**
+  * _strlen - length of string
+  * @str: string to check
+  *
+  * Return: length of string
+  */
+int _strlen(char *str)
+{
+	int len = 0;
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		len++;
+	}
+	return (len);
+}
---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+char *mul(char *num1, char *num2);
+int _puts(char *str);
+int _strlen(char *str);
+int _isdigit(char *str);
+
+#endif //HOLBERTON_H//
