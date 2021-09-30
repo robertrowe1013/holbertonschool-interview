7b84e68d82de3459fc7603d088e3ab0bb617bfc7
robertrowe1013 <robertrowe1013@gmail.com>
2021-09-30 23:06:44 +0000

    fixed for n > 3

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -10,7 +10,7 @@ def rotate_2d_matrix(matrix):
     """
     size = len(matrix) - 1
     for i in range(size):
-        for j in range(i, size):
+        for j in range(i, size - i):
             temp = matrix[i][j]
             matrix[i][j] = matrix[size - j][i]
             matrix[size - j][i] = matrix[size - i][size - j]
