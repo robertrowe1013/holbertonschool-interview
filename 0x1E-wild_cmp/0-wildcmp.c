4559d9afe1bc8b052ebc724bbdfb46a520ea93c7
robertrowe1013 <robertrowe1013@gmail.com>
2021-11-24 05:25:09 +0000

    compare wo wilds

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -15,5 +15,13 @@ int wildcmp(char *s1, char *s2)
 		return (0);
 	}
 
-	return (1);
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 2));
+	}
+	return (0);
 }
