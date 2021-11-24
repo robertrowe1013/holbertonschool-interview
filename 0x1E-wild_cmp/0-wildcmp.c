8c02a8907ffb8edef7b83ce51320bb8b9801f0e4
robertrowe1013 <robertrowe1013@gmail.com>
2021-11-24 05:16:02 +0000

    null check

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+  * wildcmp - compare two strings
+  * @s1: first string
+  * @s2: second string, may contain wildcards
+  *
+  * Return: 1 is match, else 0
+  */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (s1 == NULL || s2 == NULL)
+	{
+		return(0)
+	}
