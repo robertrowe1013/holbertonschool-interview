fd2f16f5e5afe612af93371d5c347ea9f2156b06
robertrowe1013 <robertrowe1013@gmail.com>
2021-11-24 05:37:23 +0000

    move through multiple wilds

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -19,9 +19,33 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (1);
 	}
+	if (*s2 == '*')
+	{
+		s2 = move2(s2);
+	}
 	if (*s1 == *s2)
 	{
-		return (wildcmp(s1 + 1, s2 + 2));
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
+}
+
+/**
+  * move2 - move s2
+  * @s2: pointer to s2
+  *
+  * Return: new pointer location
+  */
+
+char *move2(char *s2)
+{
+	if (*s2 == '\0')
+	{
+		return (s2);
+	}
+	if (*s2 == '*')
+	{
+		return (move2(s2 + 1));
+	}
+	return (s2);
 }
