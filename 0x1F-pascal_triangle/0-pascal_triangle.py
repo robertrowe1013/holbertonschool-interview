8011e35a7aaba42aee95613a35d1122d867b487c
robertrowe1013 <robertrowe1013@gmail.com>
2022-01-05 23:52:21 +0000

    0x1F

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Returns list of lists of pascals triangle"""
+
+
+def pascal_triangle(n):
+    """ n = height of triangle """
+    triangle = []
+    if n <= 0:
+        return triangle
+    for row in range(n):
+        col = [None for i in range(col + 1)]
+        col[0], col[-1] = 1, 1
+        for i in range(1, len(col) - 1):
+            col[i] = triangle[row - 1][i - 1] + triangle[row - 1][i]
+        triangle.append(col)
+    return triangle
