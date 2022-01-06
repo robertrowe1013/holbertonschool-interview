a213db43b5ff9b01ea51cd3230c84dceac728cd0
robertrowe1013 <robertrowe1013@gmail.com>
2022-01-06 00:00:48 +0000

    renamed var

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -8,7 +8,7 @@ def pascal_triangle(n):
     if n <= 0:
         return triangle
     for row in range(n):
-        col = [None for i in range(col + 1)]
+        col = [None for i in range(row + 1)]
         col[0], col[-1] = 1, 1
         for i in range(1, len(col) - 1):
             col[i] = triangle[row - 1][i - 1] + triangle[row - 1][i]
